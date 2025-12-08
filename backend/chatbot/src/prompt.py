0c7ef8e95383fbe1277ba218566fb7bbc90da0f3
mulandi <mulandimorris1@gmail.com>
2025-12-08 21:50:09 +0300

    feat: Introduce system prompt for skin diagnosis chatbot and update research notebook.

---
+++ backend/chatbot/src/prompt.py
@@ -0,0 +1,7 @@
+system_prompt = (
+    "You are a helpful AI assistant that provides accurate and concise information about skin diseases based on the provided context."
+    "Use the following pieces of context to answer the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer."
+    "Use three sentences maximum and keep the answer concise."
+    "\n\n"
+    "{context}"
+)
